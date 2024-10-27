14379d2d5f07aba4b47669ac4b9b63ea8c8d1260
Dylan Aviles <davil544@gmail.com>
2024-10-26 23:29:30 -0400

    Details page now loads info from the Vault

--- EntryDetails.aspx
+++ EntryDetails.aspx
@@ -1,6 +1,31 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EntryDetails.aspx.cs" Inherits="CujoPasswordManager.EntryDetails" %>
+﻿<%@ Page Title="Entry Details" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EntryDetails.aspx.cs" Inherits="CujoPasswordManager.EntryDetails" %>
 <asp:Content ID="HeadContent" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="BodyContent" ContentPlaceHolderID="body" runat="server">
-    Password Information Goes Here
+    <div align="center" style="padding-top: 10px;">
+        <table>
+            <tr>
+                <td><h3>Item Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h3></td>
+                <td><h5><asp:Label ID="lblName" runat="server"></asp:Label></h5></td>
+            </tr>
+            <tr>
+                <td><h3>Username:</h3></td>
+                <td><h5><asp:Label ID="lblUser" runat="server"></asp:Label></h5></td>
+            </tr>
+            <tr>
+                <td><h3>Password</h3></td>
+                <td><h5><asp:LinkButton ID="lnkPassword" runat="server" OnClick="LnkPassword_Click"><i>Show Password</i></asp:LinkButton></h5></td>
+            </tr>
+            <tr>
+                <td><h3>Category:</h3></td>
+                <td><h5><asp:Label ID="lblCat" runat="server"></asp:Label></h5></td>
+            </tr>
+            <tr>
+                <td><h3>Notes:</h3></td>
+                <td><h5><asp:Label ID="lblNotes" runat="server"></asp:Label></h5></td>
+            </tr>
+        </table>
+        <br /><asp:Button ID="btnEdit" runat="server" Text="Edit Entry" OnClick="BtnEdit_Click" />
+        &nbsp;<asp:Button ID="btnDelete" runat="server" Text="Delete Entry" />
+    </div>
 </asp:Content>
--- EntryDetails.aspx.cs
+++ EntryDetails.aspx.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using CujoPasswordManager.DataAccessLayer;
+using CujoPasswordManager.DataModels;
+using System;
 
 namespace CujoPasswordManager
 {
     public partial class EntryDetails : System.Web.UI.Page
     {
+        private Account account;    private Vault entry;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["account"] == null)
+            {
+                Session["entryID"] = null;
+                Response.Redirect("/Login.aspx");
+            }
+            else if (Request.QueryString["ID"] == "" || Request.QueryString["ID"] == null)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "CallMyFunction", "window.alert('No entry selected, redirecting to the home page...');", true);
+                Response.Redirect("/");
+            }
+            else
+            {
+                account = (Account)Session["account"];
+                string entryID = Request.QueryString["ID"];
+                entry = AccountManager.GetVault(account.ID, int.Parse(entryID));
 
+                lblName.Text = entry.ItemName;
+                lblUser.Text = entry.Username;
+                lblCat.Text = entry.Category;
+                lblNotes.Text = entry.Notes;
+            }
+        }
+
+        protected void LnkPassword_Click(object sender, EventArgs e)
+        {
+            //toggle show password here
+            string placeholder = "<i>Show Password</i>";
+            if (lnkPassword.Text.Equals(placeholder))
+            {
+                lnkPassword.Text = entry.Password;
+            }
+            else
+            {
+                lnkPassword.Text = placeholder;
+            }
+        }
+
+        protected void BtnEdit_Click(object sender, EventArgs e)
+        {
+            //pass entry data to add page via Add Entry page, possibly make a second form for edits
+            entry = (Vault)Session["vaultEntry"];
+            Response.Redirect("/AddEntry.aspx");
         }
     }
 }
